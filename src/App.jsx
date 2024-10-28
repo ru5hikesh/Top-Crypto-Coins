31b1a61c564f30b465b42bed576ba3736453f84c
rushi <Rushikesh10kolge@gmail.com>
2024-10-28 11:59:25 +0530

    Change in Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Coins from './Components/Coins';
 import Navbar from './Components/Navbar';
-import News from './Components/News'; // Import a News component
+import News from './Components/News';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Coins />} />
         <Route path="/home" element={<Coins />} />
         <Route path="/news" element={<News />} />
       </Routes>
